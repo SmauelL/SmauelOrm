116cc32186abc4598c3f36753a0a117a2b2acd7d
SmauelL <ljxlm502661058@163.com>
2020-11-26 10:43:43 +0800

    update,delete,count

--- session/record_test.go
+++ session/record_test.go
@@ -1,6 +1,8 @@
 package session
 
-import "testing"
+import (
+	"testing"
+)
 
 var (
 	user1 = &User{"Tom", 18}
@@ -8,9 +10,13 @@ var (
 	user3 = &User{"Jack", 25}
 )
 
+func NewSessions() *Session {
+	return New(TestDB, TestDial)
+}
+
 func testRecordInit(t *testing.T) *Session {
 	t.Helper()
-	s := NewSession().Model(&User{})
+	s := NewSessions().Model(&User{})
 	err1 := s.DropTable()
 	err2 := s.CreateTable()
 	_, err3 := s.Insert(user1, user2)
@@ -33,5 +39,35 @@ func TestSession_Find(t *testing.T) {
 	var users []User
 	if err := s.Find(&users); err != nil || len(users) != 2 {
 		t.Fatal("failed to query all")
+	}
+}
+
+func TestSession_Limit(t *testing.T) {
+	s := testRecordInit(t)
+	var users []User
+	err := s.Limit(1).Find(&users)
+	if err != nil || len(users) != 1 {
+		t.Fatal("failed to query with limit condition")
+	}
+}
+
+func TestSession_Update(t *testing.T) {
+	s := testRecordInit(t)
+	affected, _ := s.Where("Name = ?", "Tom").Update("Age", 30)
+	u := &User{}
+	_ = s.OrderBy("Age DESC").First(u)
+
+	if affected != 1 || u.Age != 30 {
+		t.Fatal("failed to update")
+	}
+}
+
+func TestSession_DeleteAndCount(t *testing.T) {
+	s := testRecordInit(t)
+	affected, _ := s.Where("Name = ?", "Tom").Delete()
+	count, _ := s.Count()
+
+	if affected != 1 || count != 1 {
+		t.Fatal("failed to delete or count")
 	}
 }
